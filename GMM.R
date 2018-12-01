2ba9811ff3db52d6e1c3d497f5b73f62fbd7378a
Dan Guo <guo.dan@husky.neu.edu>
2018-12-01 16:29:18 -0500

    gmm+k_dgmm

---
+++ GMM.R
@@ -0,0 +1,123 @@
+
+####################step 1
+
+###########fit Gaussina mixture model and obtain k based on BIC cretia
+
+library(Cardinal)
+library("mclust")
+GMM<-function(msset=msset,f=f,kmax=kmax,out=out,kprior=0)
+{
+  
+  lod<-0
+  x<-spectra(msset)[f,]
+  ###leave out pixels with intensity lower than LOD and outliers
+  if (length(x[x==min(x)])/ncol(msset)>0.01)
+  {
+    lod<-1
+    x[x==min(x)]<-NA
+    x2<-x[x<quantile(x)[3]+out*(quantile(x)[4]-quantile(x)[2])]
+
+  } else
+  {
+    x2<-x[x<quantile(x)[3]+out*(quantile(x)[4]-quantile(x)[2])]
+  }
+  #####
+  if (kprior==0)
+  {
+    gmm<-densityMclust(x2,modelNames="V")
+    if (length(gmm$BIC)>2)
+    {
+      Di<-rep(NA,length(gmm$BIC)-1)
+      for (i in 2:length(gmm$BIC))
+      {
+        Di[i-1]<-gmm$BIC[i]-gmm$BIC[i-1]
+      }
+      if (length(min(which(abs(Di)<10)))!=0)
+      {
+        k<-min(which(abs(Di)<10))
+      } else
+      {
+        k<-gmm$G
+      }
+    } else
+    {
+      k<-gmm$G
+    }
+    if (lod==0)
+    {
+      k<-min(k,kmax)
+    } else
+    {
+      k<-min(k,kmax-1)
+    }
+  } else
+  {
+    k=kprior
+  }
+  gmm<-densityMclust(x2,G=k,modelNames="V")
+  plot(gmm,what="density",x2,breaks=50)
+  aa<-rep(NA,ncol(msset))
+  x<-spectra(msset)[f,]
+  if (lod==1)
+  {
+   aa[x==NA]<-NA
+  }
+  aa[x<quantile(x)[3]+out*(quantile(x)[4]-quantile(x)[2])]<-apply(gmm$z,1, function (x) which(x==max(x)))
+  msset$gmm<-aa
+  image(msset, formula = gmm~x*y,main=paste0("feature",f))
+  
+  return(gmm)
+}
+
+
+
+########################
+#################
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+#################mouse brain
+
+load("pnnl_peaksTIC-new.rdata")
+pnnlCropped<-pnnl.peaksTIC_1
+rm(pnnl.peaksTIC_1)
+selectedSamples <- c("c1_1", "c1_2", "c1_3", 
+                     "z6_1", "z6_2", "z6_3")
+
+pnnlCropped <- pnnlCropped[,pnnlCropped$sample %in% selectedSamples]
+pnnlCropped$mouse <- factor(substr(pnnlCropped$sample, 1, 2))
+ap<-rowMeans(spectra(pnnlCropped))
+featurelist<-which(ap>sort(ap,decreasing = T)[41])
+ncomp<-matrix(0,nrow=nrow(pnnlCropped),ncol=6)
+cm<-list()
+diff<-rep(0,nrow(pnnlCropped))
+kmax=3
+out=3
+for (i in 1:nrow(pnnlCropped))
+{
+  f<-featurelist[i]
+  k<-rep(1,6)
+  for (s in 1:6)
+  {
+    msset<-pnnlCropped[,pnnlCropped$sample==unique(pnnlCropped$sample)[s]]
+    gmm<-GMM(msset=msset,f=f,kmax=kmax,out=out)
+    ncomp[f,s]<-gmm$G
+  
+  }
+  if (length(unique(ncomp[f,]))!=1)
+  {
+    diff[i]=1
+  }
+  k=Mode(k)
+  cm[[f]]<-matrix(0,ncol=k,nrow=6)
+  for (s in 1:6)
+  {
+    msset<-pnnlCropped[,pnnlCropped$sample==unique(pnnlCropped$sample)[s]]
+    gmm<-GMM(msset=msset,f=f,kmax=kmax,out=out,k_prior=k)
+    cm[[f]][s,]<-gmm$parameters$mean
+    
+  }
+  
+}
+
---
+++ k_DGMM.R
@@ -0,0 +1,184 @@
+####################step 2
+
+
+
+
+K_DGMM<-function(msset=msset,gmm=gmm,f=f)
+{
+  kr<-rep(0,8)
+  for (radius in 1:8)
+  {
+    ##################weight matrix
+    coords<-coord(msset)
+    w <- apply(coords, 1, function(pt)
+      (abs(as.numeric(pt["y"]) - as.numeric(coords[,'y'])) <= radius) & (abs(as.numeric(pt["x"]) - as.numeric(coords[,"x"])) <= radius))
+    diag(w) <- F
+    w<-apply(w,1,as.numeric)
+    
+    
+    
+    ###################################fit DGMM candidate
+    int<-spectra(msset)[f,]
+    gmm<-densityMclust(int,G=3,modelNames="V")
+    x<-int
+    N=length(x)
+    k<-gmm$G
+    g<-k
+    #mu=c(99,130);
+    #sigma=c(147,210);
+    mu<-gmm$parameters$mean
+    #mu<-c(30,60,90)
+    sigma<-gmm$parameters$variance$sigmasq
+    #sigma<-c(50,80,100)
+    alpha=rep(1,g);
+    beta=1;
+    eta<-min(mu)/1e3
+    dmu<-rep(1,g)
+    dsg<-rep(1,g)
+    dalpha<-rep(1,g)
+    ##calculate priors
+    y<-matrix(0, nrow=N, ncol=K)
+    PI<-matrix(1/K, nrow=N, ncol=K)
+    logPI<-matrix(1/K, nrow=N, ncol=K)
+    px<-matrix(0, nrow=N, ncol=K)
+    logpx<-matrix(0, nrow=N, ncol=K)
+    iteration=100
+    mutrace<-matrix(0,ncol=K,nrow=iteration)
+    sigtrace<-matrix(0,ncol=K,nrow=iteration)
+    alphatrace<-matrix(0,ncol=K,nrow=iteration)
+    betatrace<-matrix(0,ncol=1,nrow=iteration)
+    loglik<-rep(0,iteration)
+    ##p(x|\theta)
+    for (j in 1:K)
+    {
+      px[,j]<-1/(2* pi )^0.5*1/sigma[j]^0.5*exp(-(x-mu[j])^2/2/sigma[j])
+    }
+    ##logp(x|\theta)
+    for (j in 1:K)
+    {
+      logpx[,j]<-log(1/(2* pi )^0.5*1/sigma[j]^0.5)-(x-mu[j])^2/2/sigma[j]
+    }
+    
+    y<-px*PI/rowSums(px*PI)
+    y[y[,1]==0,1]<-rep(1e-200, length(y[y[,1]==0,1]))
+    y[y[,2]==0,2]<-rep(1e-200, length(y[y[,2]==0,2]))
+    
+    for (i in 1:iteration)
+    {
+      
+      
+      
+      ybar<-w%*%y/rowSums(w)
+      # ybar[ybar[,1]==0,1]<-rep(1e-100, length(ybar[ybar[,1]==0,1]))
+      # ybar[ybar[,2]==0,2]<-rep(1e-100, length(ybar[ybar[,2]==0,2]))
+      ybar[ybar==0]<-1e-100
+      loglik[i]<--sum(log(rowSums(t(t((ybar)^beta)*alpha^2)/rowSums(t(t((ybar)^beta)*alpha^2))*px)))
+      logybar<-log(ybar)
+      for ( j in 1:K)
+      {
+        logPI[,j]<-2*log(abs(alpha[j]))+beta*logybar[,j]
+      }
+      logPI<-logPI-rowMin(logPI)
+      #  logPI<-pmin(logPI,100)
+      #  logPI<-exp(logPI)
+      #  PI<-logPI/rowSums(logPI)
+      for ( j in 1:K)
+      {
+        PI[,j]<-alpha[j]^2*ybar[,j]^beta
+      }
+      PI[PI==Inf]<-1e100
+      PI<-PI/rowSums(PI)
+      
+      PI[PI==0]<-1e-100 
+      ##p(x|\theta)
+      for (j in 1:K)
+      {
+        px[,j]<-1/(2* pi )^0.5*1/sigma[j]^0.5*exp(-(x-mu[j])^2/2/sigma[j])
+      }
+      ##logp(x|\theta)
+      for (j in 1:K)
+      {
+        logpx[,j]<-log(1/(2* pi )^0.5*1/sigma[j]^0.5)-(x-mu[j])^2/2/sigma[j]
+      }
+      ##posterior
+      
+      
+      y<-px*PI/rowSums(px*PI)
+      #  y[y[,1]==0,1]<-rep(1e-200, length(y[y[,1]==0,1]))
+      #  y[y[,2]==0,2]<-rep(1e-200, length(y[y[,2]==0,2]))
+      y[y==0]<-1e-100
+      for ( j in 1:K)
+      {
+        dmu[j]<-sum(y[,j]*1/sigma[j]*(mu[j]-x))
+        dsg[j]<-1/2*sum(y[,j]*(1/sigma[j]-(x-mu[j])^2/(sigma[j]^2)))
+        dalpha_p<-y*(ybar[,j])^beta/rowSums(t(t((ybar)^beta)*alpha^2))
+        dalpha_p[is.na(dalpha_p)]<-1
+        dalpha[j]<--sum(2*y[,j]/alpha[j])+2*alpha[j]*sum(dalpha_p)
+        
+      }
+      
+      dbeta=sum(y*(-log(ybar)+rowSums(t(t((ybar)^beta)*alpha^2)*log(ybar))/rowSums(t(t((ybar)^beta)*alpha^2))))
+      mu<-mu-eta*dmu
+      sigma<-sigma-eta*dsg
+      sigma[sigma<=0]<-0.006327605
+      alpha<-alpha-eta*dalpha
+      beta<-beta-eta*dbeta
+      beta<-max(beta,0)
+      beta<-min(beta,10)
+      mutrace[i,]<-mu
+      sigtrace[i,]<-sigma
+      alphatrace[i,]<-alpha
+      betatrace[i,]<-beta
+      
+    }
+    msset$dgmm<-apply(y,1, function (x) which(x==max(x)))
+    image(msset, formula = dgmm~x*y,asp=5,colorkey=F)
+    kprim<-length(unique(msset$dgmm))
+    L<-unique(msset$dgmm)
+    for (i in L)
+    {
+      if (length(msset$dgmm[msset$dgmm==i])/ncol(msset)<0.05)
+      {
+        kprim<-kprim-1
+        msset$dgmm[msset$dgmm==i]<-NA
+      }
+      
+    }
+    print(kprim)
+    print(min(loglik))
+    print(mu)
+    seg<-unique(msset$dgmm[!is.na(msset$dgmm)])
+    seg2<-seg
+    for (i in 1:length(seg[!is.na(seg)]))
+    {
+      j<-i+1;
+      while(j<length(seg[!is.na(seg)]))
+      {
+        if ((mu[seg[i]]-mu[seg[j]])/sqrt(sigma[seg[i]]/2+sigma[seg[j]]/2)<1)
+        {
+          seg2[j]<-NA
+        }
+        j=j+1
+      }
+    }
+    kr[radius]<-length(seg2[!is.na(seg)])
+  }
+  kr<-Mode(kr)
+  print(kr)
+  return(kr)
+}
+
+
+############################mouse brain
+
+load("pnnl_peaksTIC-new.rdata")
+pnnlCropped<-pnnl.peaksTIC_1
+rm(pnnl.peaksTIC_1)
+selectedSamples <- c("c1_1", "c1_2", "c1_3", 
+                     "z6_1", "z6_2", "z6_3")
+
+pnnlCropped <- pnnlCropped[,pnnlCropped$sample %in% selectedSamples]
+pnnlCropped$mouse <- factor(substr(pnnlCropped$sample, 1, 2))
+
+msset<-pnnlCropped[,pnnlCropped$sample==unique(pnnlCropped$sample)[1]]
+kr<-K_DGMM(msset=msset,gmm=gmm,f=321)
